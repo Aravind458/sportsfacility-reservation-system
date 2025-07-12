fa4af3e1af63460a642c5e8be15d8ea93d696ad1
web-dev <sagararavind87@gmail.com>
2025-07-12 10:47:53 +0530

    changes

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,12 @@ dotenv.config();
 connectDB();
 
 const app = express();
-app.use(cors());
+const cors = require('cors');
+app.use(cors({
+  origin: 'http://localhost:5173', // your frontend port
+  credentials: true
+}));
+
 app.use(express.json());
 
 app.use('/api/users', require('./routes/users'));
